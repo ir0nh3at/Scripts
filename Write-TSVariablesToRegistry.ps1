e2ee31e549f807a1860073f5cefc274de9050e27
Steve Custer <Steve.Custer@gmail.com>
2018-08-10 15:26:23 -0700

    Added Write-TSVariablesToRegistry.ps1

---
+++ Write-TSVariablesToRegistry.ps1
@@ -0,0 +1,35 @@
+﻿#Write-TSVariablesToRegistry.ps1
+#Saves TS variables to registry.  Useful for future troubleshooting and targeting.
+#Notably the _SMSTSPackageName which will have the version info of the TS.
+
+$tsenv = New-Object -ComObject Microsoft.SMS.TSEnvironment #Create COM Object
+# $tsvars = $tsenv.GetVariables() #Put TS variable names into variable. Uncomment to get *all* variables (slows the script).
+
+# these are the ones we're actually interested in, in general:
+$tsvars = "_OSDOSImagePackageId",
+"_OSDProtectHardLinkStore",
+"_SMSTSAdvertID",
+"_SMSTSBootImageID",
+"_SMSTSLogPath",
+"_SMSTSMachineName",
+"_SMSTSMP",
+"_SMSTSPackageName",
+"IsVirtual",
+"IsLaptop",
+"IsDesktop",
+"_SMSTSUserStarted",
+"OSArchitecture",
+"OSDAdapter0MACAddress",
+"OSDAdapter0Name",
+"OSDComputerName"
+
+$registryPath = "HKLM:\Software\OSD" #This is the key the values will be written to.
+
+if (!(Test-Path $registryPath))
+        {New-Item -Path $registryPath -Force} # Check if InstallFlags path exists and creates if not.
+  
+#Iterate through and write a value for each TS variable
+foreach ($var in $tsvars)
+ {
+  New-ItemProperty -Path $registryPath -Name $var -Value $tsenv.value($var) -PropertyType String -Force
+ }
